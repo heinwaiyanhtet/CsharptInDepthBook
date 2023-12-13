f176887a458c06f09dde943b6bc366039213ec9e
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2023-12-13 13:25:36 +0630

    double yield

---
+++ double.cs
@@ -0,0 +1,33 @@
+﻿// using System;
+// using System.Collections.Generic;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         foreach (var item in GetIntegerDouble(2))
+//         {
+//             Console.WriteLine("Received: {0}", item);
+//         }
+//     }
+
+//     static IEnumerable<int> GetIntegerDouble(int count)
+//     {
+//         try
+//         {
+//             for (int i = 0; i < count; i++)
+//             {
+//                 Console.WriteLine("Yielding {0}", i);
+//                 yield return i;
+//                 int doubled = i * 2;
+//                 Console.WriteLine("Yielding {0}", doubled);
+//                 yield return doubled;
+//             }
+//         }
+//        finally
+//         {
+//         Console.WriteLine("In finally block");
+//         }
+//     }
+// }
+
